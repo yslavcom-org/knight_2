7bc694485503a39e1b52852ef8395747b7e934da
yslavcom <yslavcom@gmail.com>
2019-10-11 22:57:06 +1100

    - added destructible tank model under the tank holder TODO: finish the tank model substitutions when a vehicle gets destroyed

---
+++ Assets/_TankController/DestructibleTank/Code/SetDestructible.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;public class SetDestructible : MonoBehaviour
+{
+    Rigidbody[] rbArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbArray = GetComponentsInChildren<Rigidbody>();
+    }
+
+    int step = 0;
+    float time;
+    private void Update()
+    {
+        switch(step)
+        {
+            case 0:
+                time = Time.time;
+                step = 1;
+                break;
+
+            case 1:
+                if(Time.time -time > 5)
+                {
+                    BlowUp();
+                    step = 2;
+                }
+                break;
+
+            case 2:
+                break;
+        }
+    }
+
+
+    void BlowUp()
+    {
+        //yield return new WaitForSeconds(0.2f);
+        foreach (var rb in rbArray)
+        {
+            if(rb.name != "DestructibleTank")
+            {
+                rb.isKinematic = false;
+            }
+           //else
+           //{
+           //    rb.isKinematic = true;
+           //}
+        }
+    }
+}
