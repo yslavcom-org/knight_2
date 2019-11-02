6834edcb3c6d3e8cd5de1374aa22cf24c76b404c
yslavcom <yslavcom@gmail.com>
2019-11-02 20:15:01 +1100

    added stamina script

---
+++ Assets/_StaminaIndicators/Code/Stamina.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Stamina  : MonoBehaviour
+{
+    public abstract event Action<float> OnPctChanged;// stamina changed
+    public abstract void ModifyStamina(int amount);
+}
