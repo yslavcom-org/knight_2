7ffb22ec15ef0f43643eab6362d5477bdd0d83f9
yslavcom <yslavcom@gmail.com>
2020-05-02 21:33:37 +1000

    - at the beginning of adding bullet raycast trail via particles

---
+++ Assets/_BulletTrace/ControlMuzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMuzzle : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    float timer = 0.0f;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int seconds = (int)(timer % 60);
+        bool start_trigger = (0 == (seconds % 5))
+            && (0 != seconds);
+
+        bool stop_trigger = (0 == (seconds % 6))
+            && (0 != seconds);
+
+        if (start_trigger)
+        {
+            particleSystem.Play();
+        }
+        else if (stop_trigger)
+        {
+            particleSystem.Stop();
+        }
+    }
+}
