686b33a7bca0834809474e0cc68c589efff27d4b
yslavcom <yslavcom@gmail.com>
2020-05-18 20:58:10 +1000

    - preparing to display stats in the inventory menu

---
+++ Assets/StackedInventory/Code/StatDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatDisplay : MonoBehaviour
+{
+    public Text NameText;
+    public Text ValueText;
+
+    private void Init()
+    {
+        Text[] texts = GetComponentsInChildren<Text>();
+        NameText = texts[0];
+        ValueText = texts[1];
+    }
+
+    private void OnValidate()
+    {
+        Init();
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+}
